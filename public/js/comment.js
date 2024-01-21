9a0b2825659b60f356500202ebce91cb1e0a59a9
BenPalac1 <ben.palacpac@gmail.com>
2024-01-21 18:38:08 -0500

    updated comment form handler in comment.js

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,4 +1,28 @@
-const commentFormHandler = async function(event) {
+const commentFormHandler = async (event) => {
     event.preventDefault();
-
-};
+  
+    const postIdInput = document.querySelector('input[name="post-id"]');
+    const bodyInput = document.querySelector('textarea[name="comment-body"]');
+  
+    const postId = postIdInput.value;
+    const body = bodyInput.value;
+  
+    if (body) {
+      try {
+        await fetch('/api/comment', {
+          method: 'POST',
+          body: JSON.stringify({ postId, body }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+  
+        document.location.reload();
+      } catch (error) {
+        console.error('Error:', error.message);
+        // Handle error appropriately (e.g., show a message to the user)
+      }
+    }
+  };
+  
+  document.querySelector('#new-comment-form').addEventListener('submit', commentFormHandler);
