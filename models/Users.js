0f11b923e1670070017b42258fd20016098ed81e
BenPalac1 <ben.palacpac@gmail.com>
2024-01-21 14:39:40 -0500

    updated Users.js model to take in name, email, pword, and increment an id

---
+++ models/Users.js
@@ -0,0 +1,68 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../Connection/config'); 
+
+class User extends Model {
+    checkPassword(loginPass) {
+      return bcrypt.compareSync(loginPass, this.password);
+    }
+  }
+  
+  User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+
+      firstname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [10],
+        },
+      },
+
+    },
+    {
+      hooks: {
+        beforeCreate: async (newUserData) => {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+        beforeUpdate: async (updatedUserData) => {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+        },
+      },
+      
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user',
+    }
+  );
+  
+  module.exports = User;
