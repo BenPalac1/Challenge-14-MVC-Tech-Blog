39adced9cddee6cf2c1832f1b47ca0c20c8b57fa
BenPalac1 <ben.palacpac@gmail.com>
2024-01-21 18:04:01 -0500

    fixed syntax error

--- models/Comments.js
+++ models/Comments.js
@@ -1,7 +1,7 @@
 const {Sequelize, Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
-class Post extends Model {}
+class Comment extends Model {}
 
 Comment.init (
     {
